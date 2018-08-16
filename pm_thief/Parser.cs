5e18bdff9473310d479353e0dab2800894e35580
markiro420 <markiro420@gmail.com>
2018-08-17 00:14:17 +0300

    ^^

--- pm_thief/Parser.cs
+++ pm_thief/Parser.cs
@@ -12,7 +12,10 @@ namespace pm_thief
         public static Dictionary<string, List<Tuple<string, string>>> getSportLinksHieararchy(HtmlDocument doc)
         {
             HtmlNode table = doc.DocumentNode.SelectSingleNode("//*[@id=\"lobbySportsHolder\"]");
+            // Из тейбл нужно собрать все ссылки с названием лиг, скомпоновать в список таплов и поместить в словарь по спортам
             return new Dictionary<string, List<Tuple<string, string>>>();
         }
+
+        //public static List<>
     }
 }
