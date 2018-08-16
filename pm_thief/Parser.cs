76322375e47500f25cde2a8eb1c34faf02a90488
markiro420 <markiro420@gmail.com>
2018-08-16 23:42:14 +0300

    Adding file parser

---
+++ pm_thief/Parser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace pm_thief
+{   
+    public static class Parser
+    {
+        public static Dictionary<string, List<Tuple<string, string>>> getSportLinksHieararchy(HtmlDocument doc)
+        {
+            HtmlNode table = doc.DocumentNode.SelectSingleNode("//*[@id=\"lobbySportsHolder\"]");
+            return new Dictionary<string, List<Tuple<string, string>>>();
+        }
+    }
+}
