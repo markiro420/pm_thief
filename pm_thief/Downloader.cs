5df97be192ac23fc78a825dfbe824297bfe7e475
Arseniy Kucher <beeffallo@gmail.com>
2018-08-16 23:06:23 +0300

    downloader, welcome

---
+++ pm_thief/Downloader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace pm_thief
+{
+    static class Downloader
+    {
+        private static async Task<HtmlDocument> GetHtmlDoc(string url)
+        {
+            var client = new HttpClient();
+            var data = string.Empty;
+            try
+            {
+                data = await client.GetStringAsync(url);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                Console.WriteLine(ex.Message);
+            }
+            var doc = new HtmlDocument();
+            doc.LoadHtml(data);
+            return doc;
+        }
+
+    }
+}
