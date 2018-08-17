a6328fd6dca194fbf398c938fc2f0bc42936c4e9
Arseniy Kucher <beeffallo@gmail.com>
2018-08-17 15:21:57 +0300

    <3 x2

--- pm_thief/Parser.cs
+++ pm_thief/Parser.cs
@@ -10,7 +10,7 @@ namespace PmThief
     public static class Parser
     {
         // Маркул бля выучи нотацию
-        public static Dictionary<string, List<(string, string)>> GetSportLinksHierarchy(HtmlDocument doc)
+        public static Dictionary<string, List<(string league, string link)>> GetSportLinksHierarchy(HtmlDocument doc)
         {
             // Из тейбл нужно собрать все ссылки с названием лиг, скомпоновать в список таплов и поместить в словарь по спортам
             HtmlNode table = doc.DocumentNode.SelectSingleNode("//*[@id=\"lobbySportsHolder\"]");
@@ -21,7 +21,7 @@ namespace PmThief
             {
                 var sport = sportNode.ChildNodes["a"].InnerText;
                 var leagues_links = sportNode.ChildNodes["ul"].ChildNodes.Where(child => child.Name == "li").
-                    Select(li => (league: li.InnerText, link: li.ChildNodes["a"].GetAttributeValue("href", string.Empty)));
+                    Select(li => (li.InnerText, li.ChildNodes["a"].GetAttributeValue("href", string.Empty)));
 
                 if (!sportHierarchy.Keys.Contains(sport))
                     sportHierarchy.Add(sport, new List<(string, string)>());
